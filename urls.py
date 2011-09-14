9a6006db46e891b8337abfddd88921ae51bda586
Dominique Guardiola <contact@quinode.fr>
2011-09-14 17:47:29 +0200

    pages liens

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 handler404 = 'localsite.views.my404view'
 
-
+from django.views.generic.simple import direct_to_template
 
 from satchmo_store.urls import urlpatterns
 
@@ -10,4 +10,5 @@ from satchmo_store.urls import urlpatterns
 
 urlpatterns += patterns('',
     url(r'^admin_tools/', include('admin_tools.urls')),
+    url(r'^liens.html$', direct_to_template, {'template': 'liens.html'}),
 )
